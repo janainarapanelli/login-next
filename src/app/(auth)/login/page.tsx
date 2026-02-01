d482e782fd846274384693e90dc60e55eae07518
Janaina Rapanelli <janaina.2024silva.js@gmail.com>
2026-02-01 17:15:24 -0300

    login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,64 @@
+'use client';
+import { useState } from "react";
+
+interface LoginResponse {
+  token: string;
+  userId?: number;
+}
+
 export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Usuário ou senha inválidos");
+      }
+
+      const data: LoginResponse = await response.json();
+      console.log("Login bem-sucedido!", data);
+
+      localStorage.setItem("token", data.token);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Erro desconhecido");
+      }
+    }
+  };
+
   return (
     <main>
       <h1>Login</h1>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Login</button>
       </form>
-      </main>
-  )
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </main>
+  );
 }
