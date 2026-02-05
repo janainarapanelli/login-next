b5ef859ca31f8bb07ab1fb5e2767258f00d85f23
Janaina Silva Rapanelli <42218799+janainarapanelli@users.noreply.github.com>
2026-02-05 10:56:40 -0300

    Delete src/app/api/auth/lougout directory

--- src/app/api/auth/lougout/route.tsx
+++
@@ -1,9 +0,0 @@
-import { cookies } from 'next/headers';
-
-// Logout handler que remove o refresh token do cookie
-export async function POST() {
-  const cookieStore = await cookies();
-  cookieStore.delete('refreshToken');
-
-  return new Response(null, { status: 204 });
-}
